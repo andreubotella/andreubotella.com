3c7a0899e420a2ca1e55a28227c727014a5cc57b
Andreu Botella <andreu@andreubotella.com>
2023-03-02 11:43:01 +0100

    Add syntax highlighting

--- _config.ts
+++ _config.ts
@@ -1,9 +1,17 @@
 import lume from "lume/mod.ts";
 import metas from "lume/plugins/metas.ts";
+import { highlight as markdownHighlight } from "./_util/markdown-it-config.ts";
 
 const site = lume({
   dest: "./_out",
   location: new URL("https://andreubotella.com"),
+}, {
+  markdown: {
+    options: {
+      typographer: true,
+      highlight: markdownHighlight,
+    },
+  },
 });
 
 site.use(metas());
